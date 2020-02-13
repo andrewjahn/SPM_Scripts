586fc2d9af3bb05e3cb73f3ddf8e738d95bb8fdf
Andrew Jahn <48071253+andrewjahn@users.noreply.github.com>
2020-02-13 12:12:39 -0500

    Update RunPreproc_1stLevel_job.m

--- RunPreproc_1stLevel_job.m
+++ RunPreproc_1stLevel_job.m
@@ -10,7 +10,7 @@
 
 subjects = [01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26]; % Replace with a list of all of the subjects you wish to analyze
 
-user = getenv('USER'); % Will return the username for OSX operating systems
+user = getenv('USER'); % Will return the username for OSX operating systems; change to 'USERNAME' for Windows
 
 for subject=subjects
     
