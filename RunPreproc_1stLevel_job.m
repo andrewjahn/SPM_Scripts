e30e58d7689a8d0823b120834b92bf07ec287550
Andrew Jahn <48071253+andrewjahn@users.noreply.github.com>
2022-09-07 09:33:35 -0400

    Update RunPreproc_1stLevel_job.m

--- RunPreproc_1stLevel_job.m
+++ RunPreproc_1stLevel_job.m
@@ -21,21 +21,21 @@ subject = num2str(subject, '%02d'); % Zero-pads each number so that the subject 
 % Check whether the files have been unzipped. If not, unzip them using
 % gunzip
 
-if exist(['/Users/' user '/Desktop/Flanker/sub-' subject '/func/sub-' subject '_task-flanker_run-1_bold.nii']) == 0
+if isfile(['/Users/' user '/Desktop/Flanker/sub-' subject '/func/sub-' subject '_task-flanker_run-1_bold.nii']) == 0
     display('Run 1 has not been unzipped; unzipping now')
     gunzip(['/Users/' user '/Desktop/Flanker/sub-' subject '/func/sub-' subject '_task-flanker_run-1_bold.nii.gz'])
 else
     display('Run 1 is already unzipped; doing nothing')
 end
 
-if exist(['/Users/' user '/Desktop/Flanker/sub-' subject '/func/sub-' subject '_task-flanker_run-2_bold.nii']) == 0
+if isfile(['/Users/' user '/Desktop/Flanker/sub-' subject '/func/sub-' subject '_task-flanker_run-2_bold.nii']) == 0
     display('Run 2 has not been unzipped; unzipping now')
     gunzip(['/Users/' user '/Desktop/Flanker/sub-' subject '/func/sub-' subject '_task-flanker_run-2_bold.nii.gz'])
 else
     display('Run 2 is already unzipped; doing nothing')
 end
 
-if exist(['/Users/' user '/Desktop/Flanker/sub-' subject '/anat/sub-' subject '_T1w.nii']) == 0
+if isfile(['/Users/' user '/Desktop/Flanker/sub-' subject '/anat/sub-' subject '_T1w.nii']) == 0
     display('Anatomical image has not been unzipped; unzipping now')
     gunzip(['/Users/' user '/Desktop/Flanker/sub-' subject '/anat/sub-' subject '_T1w.nii.gz'])
 else
